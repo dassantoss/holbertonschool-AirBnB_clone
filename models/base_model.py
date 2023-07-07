d1b753fe92bb092f4fbd9c4bc383f0964e0981a9
Santiago E <santy.e@gmail.com>
2023-07-07 17:50:01 -0600

    Update init method to create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,18 @@ class BaseModel:
         common attributes and methods for other classes.
     '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initialize a new instance of BaseModel.'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, item in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        item = datetime.strptime(item, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, item)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         '''Update the updated_at attribute with the current datetime.'''
